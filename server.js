cc7d1d00d5d35fa4a2b2713c205c838c857b03d8
oncusalim <salimoncu.fb@gmail.com>
2021-05-08 21:59:38 +0300

    add initial react project and edited package.json

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ connectDB();
 app.use(express.json())
 app.use("/api", router)
 
+//production 
+if (process.env.NODE_ENV ==="production") {
+
+    app.use(express.static("client/build"))
+    app.get("*", (req,res)=>{
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
+}
+
+
 app.listen(process.env.PORT,()=>{
     console.log(`I'm listening on port ${process.env.PORT}`)
 })
